5c49ef508d29cad978e833559daf16050fb65a9c
ravindra <123213831+cypher1ravi@users.noreply.github.com>
2023-09-12 12:48:13 +0530

    error fix

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -16,10 +16,9 @@ export default function News(props) {
     }
     const updateNews = async () => {
         props.setProgress(10)
-        // const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=02fcaf7b762b4446ba95145a048d7405&page=${page}&pageSize=${props.pageSize}`;
+
         const url = `https://saurav.tech/NewsAPI/top-headlines/category/${props.category}/in.json`
         setLoader(true)
-
         let data = await fetch(url);
         props.setProgress(30)
         let parseData = await data.json();
@@ -32,8 +31,6 @@ export default function News(props) {
     }
     const fetchMoreData = async () => {
         const url = `https://saurav.tech/NewsAPI/top-headlines/category/${props.category}/in.json`
-
-        // const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=02fcaf7b762b4446ba95145a048d7405&page=${page + 1}&pageSize=${props.pageSize}`;
 
         setPage(page + 1)
         setLoader(true)
